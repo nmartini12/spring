180cceee6d5148252535bff38b4e17c3e4fb1a12
Nicolas Martini <n.martinikuro@gmail.fr>
2023-06-16 11:48:20 +0200

    spring

---
+++ dockerDemo/src/main/java/com/app/dockerdemo/DockerDemoApplication.java
@@ -0,0 +1,27 @@
+package com.app.dockerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+@SpringBootApplication
+@RestController
+public class DockerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerDemoApplication.class, args);
+    }
+
+    @GetMapping("/test")
+    public Map<String, Object> sayHello(){
+        Map<String, Object> data = new HashMap<>();
+        data.put("name","From Docker");
+        data.put("counter",10);
+        return data;
+    }
+}
